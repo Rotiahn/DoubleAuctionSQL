47f2f78b2977ecbca00b1e034f4021ee6f399f0b
Rotiahn <mail@rotiahn.com>
2023-04-18 15:31:05 -0400

    Replicate table and Insert for Seller List

--- order_generator.sql
+++ order_generator.sql
@@ -27,3 +27,24 @@ FROM
 
     ) AS buyer_bidlist
 ;
+
+
+
+
+INSERT INTO seller_order_list (seller_id,qty,price)
+SELECT 
+     seller_id
+    --,seller_bid_id
+    ,CEIL(RANDOM()*100)::INT AS qty
+    ,CEIL(RANDOM()*100)::numeric::money AS price
+FROM
+    (
+        SELECT 
+            seller_id
+            ,generate_series(1,CEIL(RANDOM()*5)::INT) AS seller_bid_id
+
+        FROM
+            (SELECT generate_series(1,10) AS buyer_id) as seller_list
+
+    ) AS seller_bidlist
+;
--- table_setup.sql
+++ table_setup.sql
@@ -14,3 +14,15 @@ CREATE TABLE buyer_order_list (
 ;
 
 
+DROP TABLE IF EXISTS seller_order_list;
+
+CREATE TABLE seller_order_list (
+     order_id   SERIAL  
+    ,seller_id  INT     NOT NULL
+    ,qty        INT     NOT NULL
+    ,price      money   NOT NULL
+
+    ,UNIQUE(seller_id,price)
+    )
+;
+
