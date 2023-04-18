016894870734a109e52b86c6081747fa9c12204b
Rotiahn <mail@rotiahn.com>
2023-04-18 17:58:36 -0400

    Aggregate multiple bids from same bidder at the same price

--- order_generator.sql
+++ order_generator.sql
@@ -13,21 +13,29 @@
 INSERT INTO buyer_order_list (buyer_id,qty,price)
 SELECT 
      buyer_id
-    --,buyer_bid_id
-    ,CEIL(RANDOM()*100)::INT AS qty
-    ,CEIL(RANDOM()*100)::numeric::money AS price
+    ,sum(qty)
+    ,price
 FROM
     (
-        SELECT 
-            buyer_id
-            ,generate_series(1,CEIL(RANDOM()*5)::INT) AS buyer_bid_id
+    SELECT 
+        buyer_id
+        --,buyer_bid_id
+        ,CEIL(RANDOM()*100)::INT AS qty
+        ,CEIL(RANDOM()*100)::numeric::money AS price
+    FROM
+        (
+            SELECT 
+                buyer_id
+                ,generate_series(1,CEIL(RANDOM()*5)::INT) AS buyer_bid_id
 
-        FROM
-            (SELECT generate_series(1,10) AS buyer_id) as buyer_list
+            FROM
+                (SELECT generate_series(1,10) AS buyer_id) as buyer_list
 
-    ) AS buyer_bidlist
-ON CONFLICT (buyer_id,price) DO UPDATE
-    SET qty = buyer_order_list.qty + excluded.qty
+        ) AS buyer_bidlist
+    ) AS agg_buyer_bidlist 
+GROUP BY
+     buyer_id
+    ,price
 --ON CONFLICT (buyer_id,price) DO UPDATE
 --    SET qty = buyer_order_list.qty + excluded.qty
 ;
@@ -38,21 +46,28 @@ ON CONFLICT (buyer_id,price) DO UPDATE
 INSERT INTO seller_order_list (seller_id,qty,price)
 SELECT 
      seller_id
-    --,seller_bid_id
-    ,CEIL(RANDOM()*100)::INT AS qty
-    ,CEIL(RANDOM()*100)::numeric::money AS price
+    ,sum(qty)
+    ,price
 FROM
     (
-        SELECT 
-            seller_id
-            ,generate_series(1,CEIL(RANDOM()*5)::INT) AS seller_bid_id
+    SELECT 
+        seller_id
+        --,seller_bid_id
+        ,CEIL(RANDOM()*100)::INT AS qty
+        ,CEIL(RANDOM()*100)::numeric::money AS price
+    FROM
+        (
+            SELECT 
+                seller_id
+                ,generate_series(1,CEIL(RANDOM()*5)::INT) AS seller_bid_id
 
-        FROM
-            (SELECT generate_series(1,10) AS seller_id) as seller_list
+            FROM
+                (SELECT generate_series(1,10) AS seller_id) as seller_list
 
-    ) AS seller_bidlist
-ON CONFLICT (seller_id,price) DO UPDATE
-    SET qty = seller_order_list.qty + excluded.qty
+        ) AS seller_bidlist
+    ) AS agg_seller_bidlist 
+GROUP BY
+     seller_id
     ,price
 --ON CONFLICT (seller_id,price) DO UPDATE
 --    SET qty = seller_order_list.qty + excluded.qty
