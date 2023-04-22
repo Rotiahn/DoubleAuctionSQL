a9f22351dfae133a2528a32724b33b9a86ed80c6
Rotiahn <mail@rotiahn.com>
2023-04-22 14:14:51 -0400

    Use Procs for table setup

--- table_setup.sql
+++ table_setup.sql
@@ -3,26 +3,26 @@
 
 DROP TABLE IF EXISTS buyer_order_list CASCADE;
 
-CREATE TABLE buyer_order_list (
-     order_id   SERIAL  
-    ,buyer_id   INT     NOT NULL
-    ,qty        INT     NOT NULL
-    ,price      money   NOT NULL
-
-    ,UNIQUE(buyer_id,price)
-    )
-;
-
+--CREATE TABLE buyer_order_list (
+--     order_id   SERIAL  
+--    ,buyer_id   INT     NOT NULL
+--    ,qty        INT     NOT NULL
+--    ,price      money   NOT NULL
+--
+--    ,UNIQUE(buyer_id,price)
+--    )
+--;
+CALL auc_create_buyorderlist('buyer_order_list');
 
 DROP TABLE IF EXISTS seller_order_list CASCADE;
 
-CREATE TABLE seller_order_list (
-     order_id   SERIAL  
-    ,seller_id  INT     NOT NULL
-    ,qty        INT     NOT NULL
-    ,price      money   NOT NULL
-
-    ,UNIQUE(seller_id,price)
-    )
-;
-
+--CREATE TABLE seller_order_list (
+--     order_id   SERIAL  
+--    ,seller_id  INT     NOT NULL
+--    ,qty        INT     NOT NULL
+--    ,price      money   NOT NULL
+--
+--    ,UNIQUE(seller_id,price)
+--    )
+--;
+CALL auc_create_sellorderlist('seller_order_list');
