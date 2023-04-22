e12bd0a2e49b7bf78aeeb924fcdeeacb5542b2df
Rotiahn <mail@rotiahn.com>
2023-04-22 15:48:59 -0400

    Added auc_findk()

--- procs.sql
+++ procs.sql
@@ -235,3 +235,109 @@ END
 
 $$ LANGUAGE PLPGSQL
 ;
+
+
+
+
+
+
+----------------------
+-- Proc name auc_findk
+-- Proc description: Finds the optimum price k that generates the most transactions
+-- Proc inputs: buyorderlist,sellorderlist
+-- Proc outputs: agg_buy,price,agg_sell,max_transact 
+-- EX: SELECT (auc_findk('buyer_order_list','seller_order_list')).*
+
+CREATE OR REPLACE FUNCTION auc_findk(
+     buyorderlist text
+    ,sellorderlist text
+)
+RETURNS 
+TABLE (
+    agg_buy INT
+    ,price money
+    ,agg_sell INT
+    ,max_transact INT
+)
+AS $$
+
+
+BEGIN
+    ASSERT auc_validate_buyorderlist(buyorderlist),   FORMAT('BuyOrderList not validated: %I',buyorderlist);
+    ASSERT auc_validate_sellorderlist(sellorderlist), FORMAT('SellOrderList not validated: %I',sellorderlist);
+
+
+    RETURN QUERY
+    EXECUTE '
+        WITH order_straddle AS (
+            SELECT
+                sum(b.qty) AS buy_qty
+                ,COALESCE(b.price,s.price) AS price
+                ,sum(s.qty) AS sell_qty
+            FROM
+                '|| buyorderlist ||' b
+                FULL OUTER JOIN '|| sellorderlist ||' s ON (b.price=s.price)
+            GROUP BY
+                COALESCE(b.price,s.price) 
+            ORDER BY 
+                COALESCE(b.price,s.price) DESC
+        )
+        , agg_straddle AS (
+        SELECT 
+            agg_demand.agg_buy
+            ,o.buy_qty
+            ,COALESCE(agg_demand.price,agg_supply.price) as price
+            ,o.sell_qty
+            ,agg_supply.agg_sell
+            ,LEAST(
+                COALESCE(agg_buy,0)
+                ,COALESCE(agg_sell,0)
+            ) AS max_transact 
+        FROM
+            order_straddle o
+            ,(--agg_demand
+                SELECT 
+                    sum(b2.buy_qty) AS agg_buy
+                    ,os.price
+                FROM
+                    order_straddle os
+                    INNER JOIN order_straddle b2 ON (b2.price>=os.price)
+                GROUP BY
+                    os.price
+            ) AS agg_demand
+            ,(--agg_supply
+                SELECT 
+                    os.price
+                    ,sum(s2.sell_qty) AS agg_sell
+                FROM
+                    order_straddle os
+                    INNER JOIN order_straddle s2 ON (s2.price<=os.price)
+                GROUP BY
+                    os.price
+            ) AS agg_supply
+        WHERE
+                agg_demand.price = agg_supply.price
+            AND o.price = agg_demand.price
+            AND o.price = agg_supply.price
+        ORDER BY 
+            price desc
+        )
+        SELECT
+            agg_buy::INT
+            ,price::money
+            ,agg_sell::INT 
+            ,max_transact::INT
+        FROM agg_straddle 
+        ORDER BY 
+            max_transact DESC 
+        LIMIT 1
+        ;
+    '
+    ;
+
+    
+    
+END
+$$ LANGUAGE PLPGSQL
+STABLE
+;
