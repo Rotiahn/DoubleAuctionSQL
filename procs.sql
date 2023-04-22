152b9a9752014d7e01c93d28c92d0e07ffa9bbf4
Rotiahn <mail@rotiahn.com>
2023-04-22 11:06:32 -0400

    Definitions auc_buyorderlist_validate & auc_sellorderlist_validate

---
+++ procs.sql
@@ -0,0 +1,11 @@
+-- Proc name: auc_buyorderlist_validate
+-- Proc description: Verify that a relation is valid and complete buy orderlist (any proc which takes a buy order list as an input should accept this buy orderlist without issues)
+-- Proc inputs: relation
+-- Proc output: boolean (true/false)
+
+
+-- Proc name: auc_sellorderlist_validate
+-- Proc description: Verify that a relation is valid and complete sell orderlist (any proc which takes a sell order list as an input should accept this sell orderlist without issues)
+-- Proc inputs: relation
+-- Proc output: boolean (true/false)
+
